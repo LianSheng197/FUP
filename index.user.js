57c14cb43ba5472cb46c87c778ac140c85cdc388
LianSheng197 <ci.i.cxcvii@gmail.com>
2021-01-26 21:56:26 +0800

    first commit

---
+++ index.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         FUP
+// @namespace    -
+// @version      0.0.1
+// @description  Unstable. No description.
+// @author       LianSheng
+// <!INCLUDES>
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+})();
